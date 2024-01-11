11a32ce6f1818d5fe9e970e173e24ffce99659b1
Justin Gardner <justin@rhynorater.com>
2024-01-11 06:03:06 -0500

    3.2.14

--- kadence-blocks/trunk/dist/components.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('kadence-helpers', 'kadence-icons', 'lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-is-shallow-equal', 'wp-keycodes', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '488ae51f7fc9f8266fc5');
--- kadence-blocks/trunk/dist/icons.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react'), 'version' => '19dac86be60081ec5d93');
