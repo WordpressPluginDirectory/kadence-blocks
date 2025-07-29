a37a7359249dbf14aa4ac51cdc94aeee995154d9
Justin Gardner <justin@rhynorater.com>
2025-07-29 06:03:08 -0500

    3.5.16

--- kadence-blocks/vendor/composer/installed.php
+++ kadence-blocks/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'kadencewp/kadence-blocks',
-        'pretty_version' => '3.5.14',
-        'version' => '3.5.14.0',
-        'reference' => '4cf97ee03a9ed777bad224ca502836c7ba9e0a7a',
+        'pretty_version' => '3.5.16',
+        'version' => '3.5.16.0',
+        'reference' => 'ae801b7ef3a25e7f02a75bbd687d4465a6c7ff07',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -47,9 +47,9 @@
             'dev_requirement' => false,
         ),
         'kadencewp/kadence-blocks' => array(
-            'pretty_version' => '3.5.14',
-            'version' => '3.5.14.0',
-            'reference' => '4cf97ee03a9ed777bad224ca502836c7ba9e0a7a',
+            'pretty_version' => '3.5.16',
+            'version' => '3.5.16.0',
+            'reference' => 'ae801b7ef3a25e7f02a75bbd687d4465a6c7ff07',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
